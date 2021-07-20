cfae0326d196addba2a61f10ef99a0af3be42bf5
William Rocha <williamferreira313@gmail.com>
2021-07-20 16:55:41 -0300

    improved the modal function and the routes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,26 +2,43 @@ import Modal from "./modal.js";
 
 const modal = Modal();
 
-const modalTitle = document.querySelector("modal h2");
-const modalDescription = document.querySelector("modal p");
-const modalButton = document.querySelector("modal button");
+const modalTitle = document.getElementById("modal-title");
+const modalDescription = document.getElementById("modal-description");
+const modalButton = document.getElementById("modal-button");
 
 //Pegar todos os botões que existem com a classe check.
 const checkButton = document.querySelectorAll(".actions a.check");
 
 checkButton.forEach((button) => {
   //Adicionar a esculta.
-  button.addEventListener("click", (event) => {
-    //Abrir modal
-    modal.open();
-  });
+  button.addEventListener("click", handleclick);
 });
 
 /*Quando o botão delete for clicado ele abre a modal */
+
 const deleteButton = document.querySelectorAll(".actions a.delete");
 
 deleteButton.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    modal.open();
-  });
+  button.addEventListener("click", (event) => handleclick(event, false));
 });
+
+function handleclick(event, check = true) {
+  event.preventDefault();
+  const text = check ? "Marcar como lida" : "Excluir";
+  const slug = check ? "check" : "delete";
+  const roomId = document.querySelector("#room-id").dataset.id;
+  const questionId = event.target.dataset.id;
+
+  const form = document.querySelector(".modal form");
+  form.setAttribute("action", `/room/${roomId}/${questionId}/${slug}`);
+
+  modalTitle.innerHTML = `${text} esta pergunta`;
+  modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`;
+  modalButton.innerHTML = `Sim, ${text.toLowerCase()}`;
+  check
+    ? modalButton.classList.remove("red")
+    : modalButton.classList.add("red");
+
+  // Abrir modal.
+  modal.open();
+}
--- src/route.js
+++ src/route.js
@@ -6,4 +6,7 @@ route.get("/", (req, res) => res.render("index"));
 route.get("/room", (req, res) => res.render("room"));
 route.get("/create-room", (req, res) => res.render("create-room"));
 
+//Formato que o formulário de dentro da modal tem que passasr
+//route.post("/room/:room/:question/:action", (req, res) => res.render(""));
+
 module.exports = route;
