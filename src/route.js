46702b158bbe22a0676ea6d19ffb21e969a7bcef
William Rocha <williamferreira313@gmail.com>
2021-07-22 14:48:47 -0300

    improve the backend rules

--- src/route.js
+++ src/route.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const QuestionController = require("./controllers/QuestionController");
 
 const route = express.Router();
 
-route.get("/", (req, res) => res.render("index"));
+route.get("/", (req, res) => res.render("index", { page: "enter-room" }));
+route.get("/create-room", (req, res) =>
+  res.render("create-room", { page: "create-room" })
+);
+
 route.get("/room", (req, res) => res.render("room"));
-route.get("/create-room", (req, res) => res.render("create-room"));
 
 //Formato que o formulário de dentro da modal tem que passasr
-//route.post("/room/:room/:question/:action", (req, res) => res.render(""));
+route.post("/question/:room/:question/:action", QuestionController.index);
 
 module.exports = route;
